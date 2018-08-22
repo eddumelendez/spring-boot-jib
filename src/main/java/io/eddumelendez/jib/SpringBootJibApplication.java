b5d425c1a4779bc4ffe286494982bf194efd1432
Eddú Meléndez <eddu.melendez@gmail.com>
2018-08-22 01:42:31 -0500

    Initial commit

---
+++ src/main/java/io/eddumelendez/jib/SpringBootJibApplication.java
@@ -0,0 +1,12 @@
+package io.eddumelendez.jib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJibApplication.class, args);
+	}
+}
